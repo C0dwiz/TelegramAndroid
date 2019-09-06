3be3884d5d28e8ad63bba170a4d80cfeb45203ab
23rd <23rd@vivaldi.net>
2019-09-06 17:03:57 +0300

    Added ability to copy Username without long touch.

--- TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
@@ -3809,6 +3809,7 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
             } else if (position == addMemberRow) {
                 openAddMember();
             } else if (position == usernameRow) {
+                if (!processCopyUsername())
                 processOnClickOrPress(position, view, x, y);
             } else if (position == locationRow) {
                 if (chatInfo.location instanceof TLRPC.TL_channelLocation) {
@@ -6037,8 +6038,7 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
         presentFragment(fragment);
     }
 
-    private boolean processOnClickOrPress(final int position, final View view, final float x, final float y) {
-        if (position == usernameRow || position == setUsernameRow) {
+    private boolean processCopyUsername() {
             final String username;
             final TLRPC.TL_username usernameObj;
             if (userId != 0) {
@@ -6149,6 +6149,11 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
                 }
             }
             return true;
+    }
+
+    private boolean processOnClickOrPress(final int position, final View view, final float x, final float y) {
+        if (position == usernameRow || position == setUsernameRow) {
+            processCopyUsername();
         } else if (position == phoneRow || position == numberRow) {
             if (editRow(view, position)) return true;
 
