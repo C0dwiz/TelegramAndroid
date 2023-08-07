58f64133bc41b109a1ad2545c1604a4b76003608
23rd <23rd@vivaldi.net>
2023-08-07 17:39:37 +0300

    Added ability to hide dialogs hint cell.

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -5828,6 +5828,16 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
         if (dialogsHintCell == null || fragmentView == null || getContext() == null) {
             return;
         }
+        dialogsHintCell.setOnLongClickListener(v -> {
+            if (dialogsHintCell != null) {
+                dialogsHintCell.setVisibility(View.VISIBLE);
+                if (fragmentView != null) {
+                    ((ContentView)fragmentView).removeView(dialogsHintCell);
+                }
+                dialogsHintCell = null;
+            }
+            return true;
+        });
         if (dialogsHintCell != null) {
             dialogsHintCell.setCompact(false);
             dialogsHintCell.setAvatars(currentAccount, null);
