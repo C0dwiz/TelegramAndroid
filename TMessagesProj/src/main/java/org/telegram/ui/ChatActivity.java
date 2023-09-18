49ba808a273131bea3f0d24bd4c92d7654f0c63e
23rd <23rd@vivaldi.net>
2023-09-18 10:40:53 +0300

    Added experimental option to delete all unpinned messages in chat.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -1661,6 +1661,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
     private final static int hideTitle = 34;
     private final static int goToFirstMessage = 35;
     private final static int deleteAllYourMessages = 36;
+    private final static int deleteAllUnpinnedMessages = 37;
 
     private final static int attach_photo = 0;
     private final static int attach_gallery = 1;
@@ -3913,6 +3914,11 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         currentAccount,
                         dialog_id,
                         getParentActivity());
+                } else if (id == deleteAllUnpinnedMessages) {
+                    org.telegram.messenger.forkgram.ForkDialogs.CreateDeleteAllUnpinnedMessagesAlert(
+                        currentAccount,
+                        dialog_id,
+                        getParentActivity());
                 }
             }
         });
@@ -4251,6 +4257,14 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     deleteAllYourMessages,
                     R.drawable.msg_delete,
                     LocaleController.getString("DeleteAllYourMessages", R.string.DeleteAllYourMessages),
+                    themeDelegate);
+            }
+            if (MessagesController.getGlobalMainSettings().getBoolean("addItemToDeleteAllUnpinnedMessages", false)
+                && ((currentUser != null && currentEncryptedChat == null) || currentChat != null)) {
+                headerItem.addSubItem(
+                    deleteAllUnpinnedMessages,
+                    R.drawable.msg_delete,
+                    LocaleController.getString("DeleteAllUnpinnedMessages", R.string.DeleteAllUnpinnedMessages),
                     themeDelegate);
             }
 
