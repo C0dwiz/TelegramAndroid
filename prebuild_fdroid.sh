cad3d9692615948c34a13985b4895d531f6d3ccf
C0dwiz <codwiz@yandex.ru>
2024-09-26 21:00:03 +0300

    deleted f-droid

--- prebuild_fdroid.sh
+++ prebuild_fdroid.sh
@@ -8,5 +8,4 @@ echo "DUMMY_CONST=0" >> $vars
 echo "ADDITIONAL_BUILD_NUMBER=$1" >> $vars
 echo "APP_ID=$2" >> $vars
 echo "APP_HASH=$3" >> $vars
-echo "F_DROID=1" >> $vars
 echo "org.gradle.workers.max=1" >> $vars
